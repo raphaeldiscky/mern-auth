8f10920af57f944ec5f8c201c4f1cd3e47cce895
Raphael Discky <zundria.putra@gmail.com>
2021-03-16 10:59:01 +0700

    Send email using nodemailer

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,14 +3,13 @@ const expressJwt = require('express-jwt')
 const _ = require('lodash')
 const axios = require('axios')
 const jwt = require('jsonwebtoken')
-const sgMail = require('@sendgrid/mail')
+const nodemailer = require('nodemailer')
 const { OAuth2Client } = require('google-auth-library')
 const { validationResult } = require('express-validator')
 
 // custom error handler to get useful error from database errors
 const { errorHandler } = require('../helpers/dbErrorHandling')
 
-sgMail.setApiKey(process.env.MAIL_KEY)
 exports.registerController = (req, res) => {
   const { name, email, password } = req.body
   const errors = validationResult(req)
@@ -40,18 +39,70 @@ exports.registerController = (req, res) => {
       { expiresIn: '15m' }
     )
 
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: 'lexie.ryan@ethereal.email',
+        pass: 'Ch2YTBnVpk8MWEytU9'
+      }
+    })
+
     // email data sending
-    const emailData = {
-      from: process.env.EMAIL_FROM,
-      to: to,
-      subject: `Account activation link`,
+    let info = {
+      from: `Fred Foo 👻 <${process.env.EMAIL_FROM}>`,
+      to: `${email}`,
+      subject: 'Account activation link ✔',
+      text: 'Hello world?',
       html: `
         <h1>Please Click to link to activate</h1>
         <p>${process.env.CLIENT_URL}/users/activate/${token}</p>
         <hr/>
         <p>This email contain sensitive info</p>
         <p>${process.env.CLIENT_URL}</p>
-      `
+    `
     }
+
+    transporter.sendMail(info, (error, info) => {
+      if (error) {
+        return res.status(400).json({
+          error: errorHandler(error)
+        })
+      } else {
+        console.log('Message sent: %s', info.messageId)
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+        return res.json({
+          message: `Email has been sent to ${email}`
+        })
+      }
+    })
   }
 }
+
+//
+
+// const emailData = {
+//   from: process.env.EMAIL_FROM,
+//   to: to,
+//   subject: `Account activation link`,
+//   html: `
+//     <h1>Please Click to link to activate</h1>
+//     <p>${process.env.CLIENT_URL}/users/activate/${token}</p>
+//     <hr/>
+//     <p>This email contain sensitive info</p>
+//     <p>${process.env.CLIENT_URL}</p>
+//   `
+// }
+
+// sgMail
+//   .send(emailData)
+//   .then((sent) => {
+//     return res.json({
+//       message: `Email has been sent to ${email}`
+//     })
+//   })
+//   .catch((err) => {
+//     return res.status(400).json({
+//       error: errorHandler(err)
+//     })
+//   })
